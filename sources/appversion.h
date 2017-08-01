4f6d3d4ad378662c8e50baa362c140e391f2dbd6
Xaviou <x.psoud@gmail.com>
2017-08-01 11:00:50 +0200

    Switched to version 1.0.0

--- sources/appversion.h
+++ sources/appversion.h
@@ -1,18 +1,18 @@
 #ifndef __APPVERSION_H_INCLUDED__
 #define __APPVERSION_H_INCLUDED__
 
-#define VERSION_MAJOR	0
-#define VERSION_MINOR	1
+#define VERSION_MAJOR	1
+#define VERSION_MINOR	0
 #define VERSION_REV     0
 // Add "201" before the number below to obtain
 // the complete date in format : YYYYMMJJ
-#define VERSION_BUILD   51028
+#define VERSION_BUILD   70801
 
 #define PRODUCTNAME "GedView"
 #define DESCRIPTION "GedView Application"
 #define wDESCRIPTION _("GedView Application")
 
-#define COPYRIGHT "Copyright (c) X.P.S.I. 2015"
-#define SMALLCOPYRIGHT "(c) X.P.S.I. 2015"
+#define COPYRIGHT "Copyright (c) X.P.S.I. 2017"
+#define SMALLCOPYRIGHT "(c) X.P.S.I. 2017"
 
 #endif // __APPVERSION_H_INCLUDED__
