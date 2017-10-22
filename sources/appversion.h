41870175ed43353e9d807551b013e92188d48148
Xaviou <x.psoud@gmail.com>
2017-10-22 18:34:35 +0200

    Switched to v1.3.0

--- sources/appversion.h
+++ sources/appversion.h
@@ -2,11 +2,11 @@
 #define __APPVERSION_H_INCLUDED__
 
 #define VERSION_MAJOR	1
-#define VERSION_MINOR	2
-#define VERSION_REV     3
+#define VERSION_MINOR	3
+#define VERSION_REV     0
 // Add "201" before the number below to obtain
 // the complete date in format : YYYYMMJJ
-#define VERSION_BUILD   70923
+#define VERSION_BUILD   71022
 
 #define PRODUCTNAME "GedView"
 #define DESCRIPTION "GedView Application"
