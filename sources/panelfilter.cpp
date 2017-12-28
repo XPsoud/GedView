6d9a7ac68b499f5c40e29b41d1dcff7b2607b100
Xaviou <x.psoud@gmail.com>
2017-12-28 18:39:16 +0100

    Reals controls in search panel

--- sources/panelfilter.cpp
+++ sources/panelfilter.cpp
@@ -1,5 +1,8 @@
 #include "panelfilter.h"
 
+#include <wx/xml/xml.h>
+#include <wx/srchctrl.h>
+
 PanelFilter::PanelFilter(wxWindow *parent) : wxPanel(parent, wxID_ANY)
 {
 #ifdef __WXDEBUG__
@@ -20,14 +23,31 @@ PanelFilter::~PanelFilter()
 
 void PanelFilter::CreateControls()
 {
+	wxStaticText *label;
+
 	wxBoxSizer *szrMain=new wxBoxSizer(wxVERTICAL);
 
-		wxStaticBoxSizer *szrStb=new wxStaticBoxSizer(wxVERTICAL, this, _("View filter:"));
+		wxStaticBoxSizer *szrStb=new wxStaticBoxSizer(wxHORIZONTAL, this, _("View filter:"));
 
-			// Fake control to see the panel "in place"
-			szrStb->Add(new wxTextCtrl(this, -1, wxEmptyString, wxDefaultPosition, wxDefaultSize, wxTE_READONLY), 1, wxALL|wxEXPAND, 5);
+			label=new wxStaticText(this, wxID_STATIC, _("String to search:"));
+			szrStb->Add(label, 0, wxALL|wxALIGN_CENTER_VERTICAL, 5);
+			m_txtSearch=new wxSearchCtrl(this, -1);
+				m_txtSearch->ShowCancelButton(true);
+			szrStb->Add(m_txtSearch, 1,  wxTOP|wxBOTTOM|wxRIGHT|wxALIGN_CENTER_VERTICAL, 5);
 
-		szrMain->Add(szrStb, 1, wxALL|wxEXPAND, 5);
+			label=new wxStaticText(this, wxID_STATIC, _("Search in:"));
+			szrStb->Add(label, 0, wxALL|wxALIGN_CENTER_VERTICAL, 5);
+
+			m_optSearch[0]=new wxRadioButton(this, -1, _("First Name"), wxDefaultPosition, wxDefaultSize, wxRB_GROUP);
+			szrStb->Add(m_optSearch[0], 0, wxTOP|wxBOTTOM|wxRIGHT|wxALIGN_CENTER_VERTICAL, 5);
+
+			m_optSearch[1]=new wxRadioButton(this, -1, _("Last Name"));
+			szrStb->Add(m_optSearch[1], 0, wxTOP|wxBOTTOM|wxRIGHT|wxALIGN_CENTER_VERTICAL, 5);
+
+			m_optSearch[2]=new wxRadioButton(this, -1, _("Full name"));
+			szrStb->Add(m_optSearch[2], 0, wxTOP|wxBOTTOM|wxALIGN_CENTER_VERTICAL, 5);
+
+		szrMain->Add(szrStb, 0, wxALL|wxEXPAND, 5);
 
 	SetSizer(szrMain);
 }
@@ -35,4 +55,9 @@ void PanelFilter::CreateControls()
 void PanelFilter::ConnectControls()
 {
 	//
+}
+
+bool PanelFilter::DoesItemMatchSearch(wxXmlNode* item)
+{
+	return true;
 }
--- sources/panelfilter.h
+++ sources/panelfilter.h
@@ -3,11 +3,15 @@
 
 #include <wx/wx.h>
 
+class wxXmlNode;
+class wxSearchCtrl;
+
 class PanelFilter : public wxPanel
 {
 	public:
 		PanelFilter(wxWindow *parent);
 		virtual ~PanelFilter();
+		bool DoesItemMatchSearch(wxXmlNode* item);
 	protected:
 	private:
 		// Misc functions
@@ -16,6 +20,8 @@ class PanelFilter : public wxPanel
         // Events handlers
         // Controls vars
 		wxStaticBox *m_stbMain;
+		wxSearchCtrl *m_txtSearch;
+		wxRadioButton *m_optSearch[3];
 		// Misc vars
 };
 
